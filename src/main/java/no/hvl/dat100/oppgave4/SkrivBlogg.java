31604842ea2fa9cebe5296f1ceebf0ffcff83c67
Vitor-dot98 <673592@stud.hvl.no>
2024-11-13 20:52:37 +0100

    update 4

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,15 +1,26 @@
 package no.hvl.dat100.oppgave4;
 
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.IOException;
 
 import no.hvl.dat100.common.TODO;
+
 import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
-
+	
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+	 String filbane = mappe + "/" + filnavn;
+	 
+	 try (PrintWriter writer = new PrintWriter(new FileWriter(filbane))){
+		 writer.print(samling.toString());
+		 return true;
+	 } catch(IOException e) {
+		 System.out.println("kunne ikke skrive til fil" + e.getMessage());
+		 return false;
+	 }
 	}
 }
+
